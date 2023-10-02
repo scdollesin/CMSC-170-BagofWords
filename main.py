74df4af4321150696fc60f7e7506bde2b5e2b2e3
scdollesin <scdollesin@up.edu.ph>
2023-10-02 22:40:59 +0800

    fix: correct display and export of data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# EXERCISE #4 BAG OF WORDS
 # AUTHOR: Samantha Shane C. Dollesin
 # STUDENT NO.: 2020-01893
 # SECTION: WX-1L
-# PROGRAM DESCRIPTION: This program takes a 
+# PROGRAM DESCRIPTION: This program takes a message from an input file and determines the frequency of each unique word in it.
 
 import re
 import tkinter as tk
@@ -26,7 +27,7 @@ words = []
 for w in raw_words:
     w = ''.join(filter(str.isalnum, w))   # remove non-alphanumeric characters
     w = w.lower()                         # convert everything into lowercase
-    if(w.isascii()):                         # disregard words with accented characters
+    if(w.isascii()):                      # disregard words with accented characters
         words.append(w)
 
 # sort the list in alphabetical order and count the frequency of the words
@@ -44,12 +45,12 @@ for word in words:
 # display and export the data from the frequency table
 output = open("output.txt","w")
 size = str(len(frequency_tb.keys()))
-total = str(len(frequency_tb.keys()))
+total = str(sum(frequency_tb.values()))
 
 print()
 print("Dictionary Size: ", size)
-output.write("Dictionary Size: " + total + "\n")
-print("Total Number of Words: ", size + "\n")
+output.write("Dictionary Size: " + size + "\n")
+print("Total Number of Words: ", total + "\n")
 output.write("Total Number of Words: " + total + "\n")
 
 for key, value in frequency_tb.items():
