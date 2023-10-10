0981b9a62f7ff2553bcfac130896e34b7de682e1
scdollesin <scdollesin@up.edu.ph>
2023-10-10 17:43:09 +0800

    feat: open spam, ham, and classify folders

--- main.py
+++ main.py
@@ -4,57 +4,68 @@
 # SECTION: WX-1L
 # PROGRAM DESCRIPTION: This program takes a message from an input file and determines the frequency of each unique word in it.
 
+import os
 import re
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askdirectory
 
-file = askopenfilename()
-input = open(file, "r")
+folders = ["Spam", "Ham", "Classify"]
 
-#Since the file contains only one message, readLine() is used without iteration
-if (input.readable()):
-    message = input.read()
+for f in folders:
+    folder = askdirectory(title= "Select "+ f + " Folder")
+    print(folder)
+    files_list = os.listdir(folder)
+    print(files_list)
 
-input.close()
+    for file in files_list:
+        file = folder+"/"+ file
+        input = open(file, "r")
 
-# Split compound words into two by replacing the '-' with a space
-message = re.sub('(?<=\w)-(?=\w)', ' ', message)
+        #Since the file contains only one message, readLine() is used without iteration
+        if (input.readable()):
+            message = input.read()
 
-# Split the message by the white spaces
-raw_words = message.split()
-words = []
+        input.close()
 
-for w in raw_words:
-    w = ''.join(filter(str.isalnum, w))   # remove non-alphanumeric characters
-    w = w.lower()                         # convert everything into lowercase
-    if(w.isascii()):                      # disregard words with accented characters
-        words.append(w)
+        # Split compound words into two by replacing the '-' with a space
+        message = re.sub('(?<=\w)-(?=\w)', ' ', message)
 
-# sort the list in alphabetical order and count the frequency of the words
-words.sort()
-frequency_tb = {}   #dictionary
+        # Split the message by the white spaces
+        raw_words = message.split()
+        words = []
 
-for word in words:
-    if (word in frequency_tb.keys()):
-        frequency_tb[word] =  frequency_tb[word] + 1
-    else:
-        frequency_tb[word] = 1
+        for w in raw_words:
+            w = ''.join(filter(str.isalnum, w))   # remove non-alphanumeric characters
+            w = w.lower()                         # convert everything into lowercase
+            #if(w.isascii()):                     
+            words.append(w)
 
-#print(frequency_tb)
+        # sort the list in alphabetical order and count the frequency of the words
+        words.sort()
+        frequency_tb = {}   #dictionary
 
-# display and export the data from the frequency table
-output = open("output.txt","w")
-size = str(len(frequency_tb.keys()))
-total = str(sum(frequency_tb.values()))
+        for word in words:
+            if (word in frequency_tb.keys()):
+                frequency_tb[word] =  frequency_tb[word] + 1
+            else:
+                frequency_tb[word] = 1
 
-print()
-print("Dictionary Size: ", size)
-output.write("Dictionary Size: " + size + "\n")
-print("Total Number of Words: ", total + "\n")
-output.write("Total Number of Words: " + total + "\n")
+        #print(frequency_tb)
 
-for key, value in frequency_tb.items():
-    output.write(key + " " + str(value) + "\n")
-    print(key, " ", value)
+        # display and export the data from the frequency table
+        #output = open("output.txt","w")
+        size = str(len(frequency_tb.keys()))
+        total = str(sum(frequency_tb.values()))
 
-output.close()
+        #print()
+        #print("Dictionary Size: ", size)
+        #output.write("Dictionary Size: " + size + "\n")
+        #print("Total Number of Words: ", total + "\n")
+        #output.write("Total Number of Words: " + total + "\n")
+
+        #for key, value in frequency_tb.items():
+            #output.write(key + " " + str(value) + "\n")
+            #rint(key, " ", value)
+
+        #print()
+        #output.close()
